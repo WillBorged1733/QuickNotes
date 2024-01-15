d4e27dc3782040bffc4360aa78578b2b28cb8b00
ayhamyza <134107458+ayhamyza@users.noreply.github.com>
2024-01-15 18:40:33 -0300

    first commit

---
+++ rocketnotes/src/Pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+import { Button } from "../../components/Button"
+
+
+export function Details () {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+      <p>Ayhamyza</p>
+      <Button/>
+    </Container>
+  )
+}
