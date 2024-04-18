496309713f60eee636eb489c14c63767b86dc56d
WillBorged1733 <134097589+WillBorged1733@users.noreply.github.com>
2024-04-18 14:59:57 -0300

    add SignIn

---
+++ rocketnotes/src/Pages/SignIn/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div  `
+    height: 100vh;
+    display: flex;
+    align-items: stretch;
+    `;
+    
+export const Form =styled.form`
+    padding: 0 136px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+    align-items: center;
+    text-align: center;
+
+    >h2{
+        padding: 48px;
+    }
+    
+`;
--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Home } from './Pages/Home'
+import { SignIn } from './Pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn/>
     </ThemeProvider>
   </React.StrictMode>,
 )
