2c8d8e04df44f2d8ceb8e29c66c4d605d6c5b044
WillBorged1733 <134097589+WillBorged1733@users.noreply.github.com>
2024-04-18 16:46:36 -0300

    update infos

---
+++ QuickNotes/src/Pages/Home/index.jsx
@@ -0,0 +1,48 @@
+import { FiPlus } from 'react-icons/fi'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+import { Note } from '../../components/Note'
+import { Input } from '../../components/Input'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>QuickNotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+        <li><ButtonText title="Todos" isActive /></li>
+        <li><ButtonText title="React" /></li>
+        <li><ButtonText title="Nodejs" /></li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo título" />
+      </Search>
+
+      <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'node.js' }
+            ]
+          }}
+          />
+        </Section>
+      </Content>
+
+      <NewNote>
+        <FiPlus />
+          Criar nota
+      </NewNote>
+    </Container>
+  )
+}
