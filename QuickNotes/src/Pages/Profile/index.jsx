b0bc9794369876acb4ecda5df10f2cbb64fd3354
WillBorged1733 <134097589+WillBorged1733@users.noreply.github.com>
2024-04-25 18:45:27 -0300

    add Avatar

--- QuickNotes/src/Pages/Profile/index.jsx
+++ QuickNotes/src/Pages/Profile/index.jsx
@@ -1,20 +1,35 @@
-import {  FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
   return (
     <Container>
       <header>
         <a href="/">
-          <FiArrowLeft/>
+          <FiArrowLeft />
         </a>
       </header>
 
       <Form>
+        <Avatar>
+          <img
+            src="https://github.com/WillBorged1733.png"
+            alt="Foto do usuário"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input
+              id="avatar"
+              type="file"
+            />
+          </label>
+        </Avatar>
+
         <Input
           placeholder="Nome"
           type="text"
